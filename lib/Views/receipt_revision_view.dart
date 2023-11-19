b25a0d0966591b655821000ee781510f03320bc4
Bence Mitlasoczki <mitlabence111@gmail.com>
2023-11-19 16:25:04 +0100

    Added move up/down functionality, removed merge down, added edit function

--- lib/Views/receipt_revision_view.dart
+++ lib/Views/receipt_revision_view.dart
@@ -4,15 +4,16 @@ import 'package:bona2/Dialogs/show_image_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
 import 'package:uuid/uuid.dart';
 import '../DataStructures/receipt.dart';
 import '../DataStructures/receipt_item.dart';
+import '../Dialogs/receipt_item_edit_dialog.dart';
 import '../constants.dart';
 import '../database_helper.dart';
 import '../post_request_provider.dart';
 import 'package:bona2/firestore_helper.dart';
 import 'package:riverpod/riverpod.dart';
-
 
 //TODO: this should replace the receiptsoverview. editing should be blocked or allowed, depending on some setting.
 const kClassName = "ReceiptRevisionView";
@@ -21,6 +22,9 @@ enum EditType {
   delete,
   mergeUp,
   mergeDown,
+  moveUp,
+  moveDown,
+  edit,
 }
 
 class EditHistoryQueueItem {
@@ -39,7 +43,8 @@ class ReceiptRevisionView extends StatefulWidget {
   /// This view should be called after a receipt and receipt items are
   /// acquired and just before inserted into the database. Review the items,
   /// details, manually correct them if necessary.
-  const ReceiptRevisionView({required this.receipt, required this.imageData, Key? key})
+  const ReceiptRevisionView(
+      {required this.receipt, required this.imageData, Key? key})
       : super(key: key);
   final Receipt receipt;
   final Uint8List? imageData;
@@ -54,14 +59,16 @@ class _ReceiptRevisionViewState extends State<ReceiptRevisionView> {
   late Provider<Receipt> receiptProvider;
   Queue<EditHistoryQueueItem> history = Queue();
 
-  int getReceiptItemsLength(ProviderRef<Receipt> ref){
+  int getReceiptItemsLength(ProviderRef<Receipt> ref) {
     return ref.read(receiptProvider).receiptItemsList.length;
   }
 
-  ReceiptItem getReceiptItem(ProviderRef<Receipt> ref, int index){
+  ReceiptItem getReceiptItem(ProviderRef<Receipt> ref, int index) {
     return ref.read(receiptProvider).receiptItemsList[index];
   }
-  void updateReceiptItem(ProviderRef<Receipt> ref, ReceiptItem newReceiptItem, int index) {
+
+  void updateReceiptItem(
+      ProviderRef<Receipt> ref, ReceiptItem newReceiptItem, int index) {
     ref.read(receiptProvider).receiptItemsList[index] = newReceiptItem;
   }
 
@@ -123,17 +130,45 @@ class _ReceiptRevisionViewState extends State<ReceiptRevisionView> {
                     Text(receipt.receiptItemsList[index].itemCategory.itemName),
                 subtitle: Text(
                     "${receipt.receiptItemsList[index].totalPrice} ${receipt.currency}"),
-                onTap: () {
+                onTap: () async {
                   //final data = ref.watch(receiptProvider);
                   // TODO: think about how to modify receipt items from a pop-up window
                   // https://stackoverflow.com/questions/54480641/flutter-how-to-create-forms-in-popup
                   // For initial value of form fields:
                   // https://stackoverflow.com/questions/43214271/how-do-i-supply-an-initial-value-to-a-text-field
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text(receipt.receiptItemsList[index].rawText ??
-                        kNullStringValue),
-                    duration: const Duration(seconds: 2),
-                  ));
+                  // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  //   content: Text(receipt.receiptItemsList[index].rawText ??
+                  //       kNullStringValue),
+                  //   duration: const Duration(seconds: 2),
+                  // ));
+                  Tuple3 editedIndexReceiptItemStatus = await showDialog(
+                    context: context,
+                    builder: (context) => ReceiptItemEditDialog(
+                        receiptItem: receipt.receiptItemsList[index],
+                        index: index),
+                  );
+                  // Save changes, add to edit history
+                  var editedIndex =
+                      editedIndexReceiptItemStatus.item1;
+                  ReceiptItem editedReceiptItem =
+                      editedIndexReceiptItemStatus.item2;
+                  EditStatus editStatus =
+                      editedIndexReceiptItemStatus.item3;
+
+                  switch(editStatus) {
+                    case EditStatus.changed:
+                      replaceAddToHistory(editedIndex, editedReceiptItem);
+                      break;
+                    // Delete is implemented in a different way, as we don't have the items in a database yet,
+                    // but in a simple List that we can modify easily.
+                    case EditStatus.deleted:
+                    case EditStatus.unchanged:
+                      break;
+                  }
+
+                  setState(() {
+                    // TODO: replace with proper update state
+                  });
                 },
                 trailing: Row(mainAxisSize: MainAxisSize.min, children: [
                   IconButton(
@@ -145,17 +180,25 @@ class _ReceiptRevisionViewState extends State<ReceiptRevisionView> {
                   ),
                   IconButton(
                     // MergeUp
-                    icon: const Icon(Icons.arrow_upward),
+                    icon: const Icon(Icons.merge_type),
                     onPressed: () {
                       mergeUpAddToHistory(index);
                       setState(() {}); // FIXME: proper way to update needed
                     },
                   ),
                   IconButton(
-                    // MergeUp
+                    // MoveUp
+                    icon: const Icon(Icons.arrow_upward),
+                    onPressed: () {
+                      moveUpAddToHistory(index);
+                      setState(() {}); // FIXME: proper way to update needed
+                    },
+                  ),
+                  IconButton(
+                    // MoveDown
                     icon: const Icon(Icons.arrow_downward),
                     onPressed: () {
-                      mergeDownAddToHistory(index);
+                      moveDownAddToHistory(index);
                       setState(() {}); // FIXME: proper way to update needed
                     },
                   ),
@@ -165,17 +208,22 @@ class _ReceiptRevisionViewState extends State<ReceiptRevisionView> {
           ),
         ),
         Text(
-            "Total: ${receipt.detectedTotalPrice} ${receipt.currency} from items, ${receipt.totalPrice} detected."),
-        IconButton(onPressed: () async {
-          if (widget.imageData != null) {
-            await showDialog(context: context, builder: (context) => ShowImageDialog(
-              imageData: widget.imageData!,
-            ),);
-          } else {
-            print("No image was found.");
-          }
-
-        }, icon: const Icon(Icons.preview),),
+            "Total: ${receipt.detectedTotalPrice.toStringAsFixed(2)} ${receipt.currency} from items, ${receipt.totalPrice.toStringAsFixed(2)} detected."),
+        IconButton(
+          onPressed: () async {
+            if (widget.imageData != null) {
+              await showDialog(
+                context: context,
+                builder: (context) => ShowImageDialog(
+                  imageData: widget.imageData!,
+                ),
+              );
+            } else {
+              print("No image was found.");
+            }
+          },
+          icon: const Icon(Icons.preview),
+        ),
       ]),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.save),
@@ -192,7 +240,8 @@ class _ReceiptRevisionViewState extends State<ReceiptRevisionView> {
               duration: const Duration(seconds: 2),
             ));
             print("Uploading to cloud...");
-            final String fname = "${UuidValue.fromByteList(receipt.uuid).uuid}.json";
+            final String fname =
+                "${UuidValue.fromByteList(receipt.uuid).uuid}.json";
             // TODO: it should be uploaded into an own (uuid) folder per user!
             await fireStoreHelper.uploadReceiptAndItems(receipt);
             //await fireStoreHelper.uploadSample();
@@ -233,6 +282,63 @@ class _ReceiptRevisionViewState extends State<ReceiptRevisionView> {
     }
   }
 
+  void moveUp(int index) {
+    /// Moves item at index up once, into index-1. Element at index-1 is moved to index.
+    /// This function does no check on index!
+    ReceiptItem item = receipt.receiptItemsList[index];
+    ReceiptItem itemAbove = receipt.receiptItemsList[index - 1];
+
+    receipt.receiptItemsList[index] = itemAbove;
+    receipt.receiptItemsList[index - 1] = item;
+  }
+
+  void moveDown(int index) {
+    /// Moves item at index down once, into index+1. Element at index+1 is moved to index.
+    /// This function does no check on index!
+    ReceiptItem item = receipt.receiptItemsList[index];
+    ReceiptItem itemBelow = receipt.receiptItemsList[index + 1];
+
+    receipt.receiptItemsList[index] = itemBelow;
+    receipt.receiptItemsList[index + 1] = item;
+  }
+
+  void moveUpAddToHistory(int index) {
+    if (index > 1) {
+      // do nothing if element is first
+      moveUp(index);
+      ReceiptItem item =
+          receipt.receiptItemsList[index - 1]; // Get the item that was moved up
+      history.addLast(EditHistoryQueueItem(
+        editType: EditType.moveUp,
+        index: index,
+        receiptItem:
+            item, // TODO: for moving up or down, no need to pass receiptItem
+      ));
+    }
+  }
+
+  void moveDownAddToHistory(int index) {
+    if (index < receipt.receiptItemsList.length - 1) {
+      // do nothing if element is last
+      moveDown(index);
+      ReceiptItem item = receipt
+          .receiptItemsList[index + 1]; // Get the item that was moved down
+      history.addLast(EditHistoryQueueItem(
+        editType: EditType.moveDown,
+        index: index,
+        receiptItem:
+            item, // TODO: for moving up or down, no need to pass receiptItem
+      ));
+    }
+  }
+
+  void replaceAddToHistory(int index, ReceiptItem newItem){
+    ReceiptItem overwrittenItem = receipt.receiptItemsList[index];
+    receipt.receiptItemsList[index] = newItem;
+    history.addLast(EditHistoryQueueItem(
+        editType: EditType.edit, index: index, receiptItem: overwrittenItem));
+  }
+
   void deleteAddToHistory(int index) {
     ReceiptItem removedItem = receipt.receiptItemsList.removeAt(index);
     history.addLast(EditHistoryQueueItem(
@@ -261,13 +367,26 @@ class _ReceiptRevisionViewState extends State<ReceiptRevisionView> {
         receipt.receiptItemsList
             .insert(historyItem.index, historyItem.receiptItem);
         return 1;
+      case EditType.moveUp:
+        // Element at index was moved up, so need to move element at index-1 down.
+        moveDown(historyItem.index - 1);
+        return 1;
+      case EditType.moveDown:
+        // Element at index was moved down, so need to move element at index+1 up.
+        moveUp(historyItem.index + 1);
+        return 1;
+      case EditType.edit:
+        // Change edited element back to original
+        receipt.receiptItemsList[historyItem.index] = historyItem.receiptItem;
+        return 1;
     }
   }
 
   int undoLastEditRemoveFromHistory() {
     if (history.isEmpty) {
       if (kDebugMode) {
-        print('${kClassName}: undoLastEditRemoveFromHistory() was called with empty history.');
+        print(
+            '${kClassName}: undoLastEditRemoveFromHistory() was called with empty history.');
       }
       return 0;
     }
