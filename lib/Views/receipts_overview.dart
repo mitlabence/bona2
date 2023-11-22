a5f57cb97a5d77817ebfc6d07184d1499f45234a
Bence Mitlasoczki <mitlabence111@gmail.com>
2023-11-22 20:05:36 +0100

    Removed "clear local databases" function, improved layout

--- lib/Views/receipts_overview.dart
+++ lib/Views/receipts_overview.dart
@@ -26,7 +26,8 @@ class _ReceiptsOverviewState extends State<ReceiptsOverview> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
         appBar: AppBar(
           title: const Text("Receipts and Items"),
           centerTitle: true,
@@ -35,78 +36,85 @@ class _ReceiptsOverviewState extends State<ReceiptsOverview> {
                 itemBuilder: (context) => [
                       PopupMenuItem<int>(
                           onTap: () async {
-                            await dbh.clearTable();
-                            setState(
-                                () {}); // TODO: there must be a better way of notifying the framework of the change! Or put changes inside
+                            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                              content: Text('Clearing local databases blocked.'),
+                              duration: Duration(seconds: 2),
+                            ));
+                            //await dbh.clearTable();
+                            // setState(
+                            //     () {}); // TODO: there must be a better way of notifying the framework of the change!
                           },
                           child: const Text("Clear local databases"))
                     ])
           ],
         ),
-        body: Column(children: [
-          SizedBox(
-            height: 500,
-            width: 300,
-            child: FutureBuilder<List<Receipt>>(
-              // TODO: with increasing number of receipts, need to make smaller queries, updating with scrolling
-              future: DataBaseHelper.instance.getReceipts(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<List<Receipt>> snapshot) {
-                if (!snapshot.hasData) {
-                  if (snapshot.hasError) {
-                    print(snapshot
-                        .error); // TODO: proper connectionState sampling logic necessary
+        body: Container(
+          constraints: BoxConstraints.expand(),
+
+          child: Column(children: [
+            Expanded(
+              flex: 10,
+              child: FutureBuilder<List<Receipt>>(
+                // TODO: with increasing number of receipts, need to make smaller queries, updating with scrolling
+                future: DataBaseHelper.instance.getReceipts(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<List<Receipt>> snapshot) {
+                  if (!snapshot.hasData) {
+                    if (snapshot.hasError) {
+                      print(snapshot
+                          .error); // TODO: proper connectionState sampling logic necessary
+                    }
+                    return const Center(child: Text('Loading...'));
+                  } else {
+                    return snapshot.data!.isEmpty
+                        ? const Center(child: Text('No receipts yet.'))
+                        : ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              return ReceiptTile(
+                                title: snapshot.data![index].shopName,
+                                subtitle:
+                                    "${formatDateTimeToMinutes(snapshot.data![index].dateTime)}, ${snapshot.data![index].totalPrice} ${snapshot.data![index].currency}",
+                                onTapCallback: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ReceiptItemListView(
+                                                receiptUuid:
+                                                    snapshot.data![index].uuid,
+                                              )));
+                                },
+                                onLongPressCallback: () async {
+                                  DataBaseHelper dbh = DataBaseHelper.instance;
+                                  await dbh.removeReceiptAndItemsByUUID(
+                                      snapshot.data![index].uuid);
+                                  setState(() {});
+                                  print("Removed receipt");
+                                },
+                              );
+                            });
                   }
-                  return const Center(child: Text('Loading...'));
-                } else {
-                  return snapshot.data!.isEmpty
-                      ? const Center(child: Text('No receipts yet.'))
-                      : ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (context, index) {
-                            return ReceiptTile(
-                              title: snapshot.data![index].shopName,
-                              subtitle:
-                                  "${formatDateTimeToMinutes(snapshot.data![index].dateTime)}, ${snapshot.data![index].totalPrice} ${snapshot.data![index].currency}",
-                              onTapCallback: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            ReceiptItemListView(
-                                              receiptUuid:
-                                                  snapshot.data![index].uuid,
-                                            )));
-                              },
-                              onLongPressCallback: () async {
-                                DataBaseHelper dbh = DataBaseHelper.instance;
-                                await dbh.removeReceiptAndItemsByUUID(
-                                    snapshot.data![index].uuid);
-                                setState(() {});
-                                print("Removed receipt");
-                              },
-                            );
-                          });
-                }
-              },
+                },
+              ),
             ),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text("Add receipt"),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text("Remove receipt"),
-          ),
-        ]),
-        floatingActionButton: FloatingActionButton(onPressed: () async {
-          Receipt r = rrg.randomReceipt(3);
-          await DataBaseHelper.instance.addReceipt(r);
-          await DataBaseHelper.instance.addReceiptItems(r.receiptItemsList);
-          setState(
-              () {}); // TODO: there must be a better way of notifying the framework of the change! Or put changes inside
-        }));
+            Expanded(
+              flex:1,
+              child: TextButton(
+                onPressed: () {},
+                child: Text("Add receipt"),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: TextButton(
+                onPressed: null,
+                child: Text("Remove receipt"),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
