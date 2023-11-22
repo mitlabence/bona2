4322d9c81d901f32db963855ba2afdf44d6e60bd
Bence Mitlasoczki <mitlabence111@gmail.com>
2023-11-22 20:03:38 +0100

    Added AppBar, zooming and panning function, skeleton setting x axis limits (in data query step).

--- lib/Views/visualization_view.dart
+++ lib/Views/visualization_view.dart
@@ -2,18 +2,17 @@ import 'package:bona2/database_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class VisualizationView extends ConsumerWidget {
+  const VisualizationView({super.key});
+
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final testAsyncValue = ref.watch(testProvider);
-    /*
-    return
-    */
 
     return Scaffold(
+      appBar: AppBar(title: Text("Visualization")),
         body: Center(
       child: testAsyncValue.when(
         data: (dataPoints) => SfCartesianChart(
@@ -25,14 +24,16 @@ class VisualizationView extends ConsumerWidget {
             // Enable tooltip
             tooltipBehavior: TooltipBehavior(enable: true),
             series: <ChartSeries<_TotalPriceData, DateTime>>[
-              LineSeries<_TotalPriceData, DateTime>(
+              FastLineSeries<_TotalPriceData, DateTime>(
                   dataSource: List.generate(dataPoints.length, (index) => _TotalPriceData(dataPoints[index]["datetime"], dataPoints[index]["totalprice"])),
                   xValueMapper: (_TotalPriceData totalPriceData, _) => totalPriceData.dateTime,
                   yValueMapper: (_TotalPriceData totalPriceData, _) => totalPriceData.totalPrice,
                   name: 'Spent9i',
                   // Enable data label
                   dataLabelSettings: DataLabelSettings(isVisible: true))
-            ]),
+            ],
+          zoomPanBehavior: ZoomPanBehavior(enablePinching: true, enablePanning: true,),
+        ),
         error: (_, __) => const Center(
           child: Text("Error"),
         ),
@@ -51,7 +52,10 @@ class VisualizationView extends ConsumerWidget {
 
 final testProvider = FutureProvider<List<dynamic>>((ref) async {
   DataBaseHelper dbh = DataBaseHelper.instance;
-  return dbh.getReceiptsDateTimeTotalPrice();
+  final DateTime now = DateTime.now();
+  final DateTime startTime = now.subtract(Duration(days: 3*30));
+
+  return dbh.getReceiptsDateTimeTotalPriceBetween(DateTime.fromMillisecondsSinceEpoch(0), now);
 });
 
 class _TotalPriceData {
