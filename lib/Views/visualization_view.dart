3ef1c91e49a7bb7496bf11e07d1ba173daa9ad75
Bence Mitlasoczki <mitlabence111@gmail.com>
2024-04-12 22:51:11 +0200

    Updated visualization to show windowed total spendings

--- lib/Views/visualization_view.dart
+++ lib/Views/visualization_view.dart
@@ -1,4 +1,5 @@
 import 'package:bona2/database_helper.dart';
+import 'package:bona2/utils/time_series_tools.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
@@ -6,41 +7,61 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 class VisualizationView extends ConsumerWidget {
   const VisualizationView({super.key});
 
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final testAsyncValue = ref.watch(testProvider);
 
     return Scaffold(
-      appBar: AppBar(title: Text("Visualization")),
+        appBar: AppBar(title: Text("Visualization")),
         body: Center(
-      child: testAsyncValue.when(
-        data: (dataPoints) => SfCartesianChart(
-            primaryXAxis: DateTimeAxis(),
-            // Chart title
-            title: ChartTitle(text: 'Spendings'),
-            // Enable legend
-            legend: Legend(isVisible: false),
-            // Enable tooltip
-            tooltipBehavior: TooltipBehavior(enable: true),
-            series: <ChartSeries<_TotalPriceData, DateTime>>[
-              FastLineSeries<_TotalPriceData, DateTime>(
-                  dataSource: List.generate(dataPoints.length, (index) => _TotalPriceData(dataPoints[index]["datetime"], dataPoints[index]["totalprice"])),
-                  xValueMapper: (_TotalPriceData totalPriceData, _) => totalPriceData.dateTime,
-                  yValueMapper: (_TotalPriceData totalPriceData, _) => totalPriceData.totalPrice,
-                  name: 'Spent9i',
-                  // Enable data label
-                  dataLabelSettings: DataLabelSettings(isVisible: true))
-            ],
-          zoomPanBehavior: ZoomPanBehavior(enablePinching: true, enablePanning: true,),
-        ),
-        error: (_, __) => const Center(
-          child: Text("Error"),
-        ),
-        loading: () => const Text("Loading"),
-      ),
-    )
-    );
+          child: testAsyncValue.when(
+            data: (dataPoints) {
+              // Generate TimeSeriesData with daily intervals, and sum of total price per day
+              List<TimeStamp> ts = List.generate(dataPoints.length,
+                  (index) => TimeStamp(dataPoints[index]["datetime"]));
+              List<num> ys = List.generate(dataPoints.length,
+                  (index) => dataPoints[index]["totalprice"]);
+              final TimeSeriesData timeSeriesData = TimeSeriesData(ts, ys)
+                  .resample(
+                      TimeStamp(DateTime(2021, 01, 01)),
+                      TimeStamp(DateTime.now()),
+                      const Duration(days: 30),
+                      (l) => l.reduce((value, element) => value + element));
+              return SfCartesianChart(
+                primaryXAxis: DateTimeAxis(),
+                // Chart title
+                title: ChartTitle(text: 'Spendings'),
+                // Enable legend
+                legend: const Legend(isVisible: false),
+                // Enable tooltip
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<_TotalPriceData, DateTime>>[
+                  FastLineSeries<_TotalPriceData, DateTime>(
+                      dataSource: List.generate(
+                          timeSeriesData.length,
+                          (index) => _TotalPriceData(
+                              timeSeriesData.t[index].dateTime,
+                              timeSeriesData.y[index].toDouble())),
+                      xValueMapper: (_TotalPriceData totalPriceData, _) =>
+                          totalPriceData.dateTime,
+                      yValueMapper: (_TotalPriceData totalPriceData, _) =>
+                          totalPriceData.totalPrice,
+                      name: 'Spent',
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ],
+                zoomPanBehavior: ZoomPanBehavior(
+                  enablePinching: true,
+                  enablePanning: true,
+                ),
+              );
+            },
+            error: (_, __) => const Center(
+              child: Text("Error"),
+            ),
+            loading: () => const Text("Loading"),
+          ),
+        ));
   }
 }
 
@@ -53,9 +74,9 @@ class VisualizationView extends ConsumerWidget {
 final testProvider = FutureProvider<List<dynamic>>((ref) async {
   DataBaseHelper dbh = DataBaseHelper.instance;
   final DateTime now = DateTime.now();
-  final DateTime startTime = now.subtract(Duration(days: 3*30));
-
-  return dbh.getReceiptsDateTimeTotalPriceBetween(DateTime.fromMillisecondsSinceEpoch(0), now);
+  final DateTime startTime = now.subtract(Duration(days: 3 * 30));
+  return dbh.getReceiptsDateTimeTotalPriceBetween(
+      DateTime.fromMillisecondsSinceEpoch(0), now);
 });
 
 class _TotalPriceData {
