d2103b1b3e7435ebcbcb1d21b1a024090aab96b1
Bence Mitlasoczki <mitlabence111@gmail.com>
2024-05-22 23:31:58 +0200

    Added skeleton button

--- lib/Views/visualization_view.dart
+++ lib/Views/visualization_view.dart
@@ -4,14 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
+
 class VisualizationView extends ConsumerWidget {
   const VisualizationView({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final testAsyncValue = ref.watch(testProvider);
 
     return Scaffold(
-        appBar: AppBar(title: Text("Visualization")),
+        appBar: AppBar(
+          title: const Text("Receipts and Items"),
+          centerTitle: true,
+          actions: [
+            PopupMenuButton(
+                itemBuilder: (context) => [
+                      PopupMenuItem<int>(
+                          onTap: () {
+                            print("pressed button in visualizatioin");
+                          },
+                          child: const Text("sample button"))
+                    ])
+          ],
+        ),
         body: Center(
           child: testAsyncValue.when(
             data: (dataPoints) {
@@ -21,8 +36,8 @@ class VisualizationView extends ConsumerWidget {
               List<num> ys = List.generate(dataPoints.length,
                   (index) => dataPoints[index]["totalprice"]);
               //String currency = dataPoints.isNotEmpty ? dataPoints[0]["currency"] : "NaN";
-              final TimeSeriesData timeSeriesData = TimeSeriesData(ts, ys, yUnit: "EUR")
-                  .resampleMonths(
+              final TimeSeriesData timeSeriesData =
+                  TimeSeriesData(ts, ys, yUnit: "EUR").resampleMonths(
                       TimeStamp(DateTime(2021, 01, 01)),
                       TimeStamp(DateTime.now()),
                       (l) => l.reduce((value, element) => value + element));
@@ -33,22 +48,29 @@ class VisualizationView extends ConsumerWidget {
                 // Enable legend
                 legend: const Legend(isVisible: false),
                 // Enable tooltip
-                tooltipBehavior: TooltipBehavior(enable: true, builder: (dynamic data, dynamic point, dynamic series, int pointIndex, int seriesIndex) {
-                  return Container(height: 50,
-                    width: 100,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                    ),
-                    // FIXME currency not working
-                    child: Text('${data.dateTime.year}.${data.dateTime.month}. - ${data.totalPrice.toStringAsFixed(2)} ${data.currency}'),);
-                }),
+                tooltipBehavior: TooltipBehavior(
+                    enable: true,
+                    builder: (dynamic data, dynamic point, dynamic series,
+                        int pointIndex, int seriesIndex) {
+                      return Container(
+                        height: 50,
+                        width: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        // FIXME currency not working
+                        child: Text(
+                            '${data.dateTime.year}.${data.dateTime.month}. - ${data.totalPrice.toStringAsFixed(2)} ${data.currency}'),
+                      );
+                    }),
                 series: <ChartSeries<_TotalPriceData, DateTime>>[
                   FastLineSeries<_TotalPriceData, DateTime>(
                     dataSource: List.generate(
                         timeSeriesData.length,
                         (index) => _TotalPriceData(
                             timeSeriesData.t[index].dateTime,
-                            timeSeriesData.y[index].toDouble(), timeSeriesData.yUnit)),
+                            timeSeriesData.y[index].toDouble(),
+                            timeSeriesData.yUnit)),
                     xValueMapper: (_TotalPriceData totalPriceData, _) =>
                         totalPriceData.dateTime,
                     yValueMapper: (_TotalPriceData totalPriceData, _) =>
